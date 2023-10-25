ab1c2c828a7559120e7301ba8869cb6cb16039f7
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-10-25 14:58:57 +0200

    ALU Tester with values

--- codeA2/src/test/scala/ALUTester.scala
+++ codeA2/src/test/scala/ALUTester.scala
@@ -3,12 +3,32 @@ import chisel3.iotesters.PeekPokeTester
 
 class ALUTester(dut: ALU) extends PeekPokeTester(dut) {
 
-  //
+  //Check that the ALU is working correctly
+  //Addition
+  poke(dut.io.op1, 1)
+  poke(dut.io.op2, 2)
+  poke(dut.io.sel, 0)
+  step(5)
+
+  //Addition
+  poke(dut.io.op1, 0)
+  poke(dut.io.op2, 0)
+  poke(dut.io.sel, 0)
+  step(5)
+
+
+  //Subtraction
   poke(dut.io.op1,321)
   poke(dut.io.op2, 320)
   poke(dut.io.sel, 1)
-
   step(5)
+
+  //Subtraction
+  poke(dut.io.op1, 321)
+  poke(dut.io.op2, 321)
+  poke(dut.io.sel, 1)
+  step(5)
+
 }
 
 object ALUTester {
