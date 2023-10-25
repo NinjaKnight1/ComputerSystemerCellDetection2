e6d861b552f47cf32f9ac583b5ca47aabe81cde9
rasmussalling <s224788@dtu.dk>
2023-10-25 14:10:07 +0200

    make alu

--- codeA2/src/main/scala/ALU.scala
+++ codeA2/src/main/scala/ALU.scala
@@ -4,8 +4,25 @@ import chisel3.util._
 class ALU extends Module {
   val io = IO(new Bundle {
     //Define the module interface here (inputs/outputs)
+    val op1 = Input(UInt(32.W))
+    val op2 = Input(UInt(32.W))
+    val sel = Input(UInt(2.W))
+    val result = Output(UInt(32.W))
+    val IsZero = Output(UInt(1.W))
   })
 
+  io.result := 0.U
+
+  switch(io.sel) {
+    is(0.U) {io.result := io.op1 + io.op2}
+    is(1.U) {io.result := io.op1 - io.op2}
+  }
+
+  io.IsZero := 0.U
+
+  switch(io.result) {
+    is(0.U) {io.IsZero := 1.U}
+  }
   //Implement this module here
 
 }
