af68f5840d94b15801eafcb70e9149851fbfe426
rasmussalling <s224788@dtu.dk>
2023-11-01 14:44:38 +0100

    Update ControlUnit.scala

--- codeA2/src/main/scala/ControlUnit.scala
+++ codeA2/src/main/scala/ControlUnit.scala
@@ -6,12 +6,16 @@ class ControlUnit extends Module {
     //Define the module interface here (inputs/outputs)
     val opCode = Input(UInt(6.W))
 
-    val ALUsel = Output(UInt(1.W))
-    val ALUsrc = Output(UInt(1.W))
+    val isIm = Output(UInt(1.W))
+    val ALUopr = Output(UInt(1.W))
+    val isImALU = Output(UInt(1.W))
+    val branchCond = Output(UInt(1.W))
+    val regChange = Output(UInt(1.W))
     val branch = Output(Bool ())
-    val stop = Output(Bool ())
-    val run = Output(Bool ())
-    val writeEnable = Output(UInt(1.W))
+    val END = Output(Bool ())
+    val memWriteEnable = Output(Bool ())
+    val fromMem = Output(UInt(1.W))
+    val regWriteEnable = Output(UInt(1.W))
 
   })
 
@@ -20,8 +24,16 @@ class ControlUnit extends Module {
   val cat = Cat(io.opCode(2), io.opCode(3))
   val op = Cat(io.opCode(4), io.opCode(5))
 
-  io.ALUsel := 0.U
-  io.writeEnable := 0.U
+  io.isIm := 0.U
+  io.ALUopr := 0.U
+  io.isImALU := 0.U
+  io.branchCond := 0.U
+  io.regChange := 0.U
+  io.branch := 0.U
+  io.END := 0.U
+  io.memWriteEnable := 0.U
+  io.fromMem := 0.U
+  io.regWriteEnable := 0.U
 
   switch(im){
     is(0.U){/*No operation*/}
@@ -30,28 +42,30 @@ class ControlUnit extends Module {
       switch(cat){
         is(1.U){
           //Arithmetic operation
-          io.writeEnable := 1.U
+          io.regWriteEnable := 1.U
           switch(op){
             is(1.U){
               /*Addition (ADD)*/
-              io.ALUsel := 0.U
+              io.ALUopr := 0.U
             }
             is(2.U){
               /*Subtraction (SUB)*/
-              io.ALUsel := 1.U
+              io.ALUopr := 1.U
             }
           }
         }
         is(2.U){
           //Data operation
+          io.regChange := 1.U
           switch(op){
             is(1.U){
               /*Load data (LD)*/
-              io.writeEnable := 1.U
+              io.regWriteEnable := 1.U
+              io.fromMem := 1.U
             }
             is(2.U){
               /*Store data (SD)*/
-              io.writeEnable := 0.U
+              io.memWriteEnable := 1.U
             }
           }
         }
@@ -62,21 +76,41 @@ class ControlUnit extends Module {
       switch(cat){
         is(1.U){
           //Arithmetic operation
+          io.isImALU := 1.U
+          io.regWriteEnable := 1.U
           switch(op){
-            is(1.U){/*Immediate Addition (ADDI)*/}
-            is(2.U){/*Immediate Subtraction (SUBI)*/}
+            is(1.U){
+              /*Immediate Addition (ADDI)*/
+              io.ALUopr := 0.U
+            }
+            is(2.U){
+              /*Immediate Subtraction (SUBI)*/
+              io.ALUopr := 1.U
+            }
           }
         }
         is(2.U){
           //Load immediate (LI)
+          io.regWriteEnable := 1.U
+          io.isIm := 1.U
         }
       }
     }
     is(3.U){
       switch(op){
-        is(1.U){/*Jumping to branch (BRANCH)*/}
-        is(2.U){/*If it is zero (BZERO)*/}
-        is(3.U){/*End execution (END)*/}
+        is(1.U){
+          /*Jumping to branch (BRANCH)*/
+          io.branch := 1.U
+        }
+        is(2.U){
+          /*If it is zero (BEQ)*/
+          io.regChange := 1.U
+          io.branchCond := 1.U
+        }
+        is(3.U){
+          /*End execution (END)*/
+          io.END := 1.U
+        }
       }
     }
   }
